b8ce1f90609346dcd37dd49bc85584d6ffdab4ee
Austin Lamb <arlamb@usc.edu>
2023-12-06 18:24:50 -0800

    Fix all train files to be correct.

--- experiments_PureT/PureT_SwinV2_XE/train.sh
+++ experiments_PureT/PureT_SwinV2_XE/train.sh
@@ -1,4 +1,4 @@
-# CUDA_VISIBLE_DEVICES=0 python main.py --folder ./experiments_PureT/PureT_SwinV2_XE/
-CUDA_VISIBLE_DEVICES=0 python main.py --folder ./experiments_PureT/PureT_SwinV2_XE/ --resume 9 --load_epoch
+CUDA_VISIBLE_DEVICES=0 python main.py --folder ./experiments_PureT/PureT_SwinV2_XE/
+# CUDA_VISIBLE_DEVICES=0 python main.py --folder ./experiments_PureT/PureT_SwinV2_XE/ --resume 9 --load_epoch
 
 # CUDA_VISIBLE_DEVICES=0, 1 python -m torch.distributed.launch --master_port=3142 --nproc_per_node=2 main_multi_gpu.py --folder ./experiments_PureT/PureT_XE/
